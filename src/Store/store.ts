29d00fe34c18b7a69b04b70df643bbcde2245c7b
Tejasvi Raj Pant <tejpant@gmail.com>
2020-06-12 17:28:39 +0545

    Created store

---
+++ src/Store/store.ts
@@ -0,0 +1,12 @@
+import { applyMiddleware, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import rootReducer from '../Reducers/weatherReducer';
+import { fetchWeatherWatcher } from '../Saga/saga';
+import initialState from './initialState'
+
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(rootReducer, initialState, applyMiddleware(sagaMiddleware))
+sagaMiddleware.run(fetchWeatherWatcher)
+
+export default store

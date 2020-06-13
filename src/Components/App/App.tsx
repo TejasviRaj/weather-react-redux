44c75dde2fe42541b86c8808998c12976c552b5c
Tejasvi Raj Pant <tejpant@gmail.com>
2020-06-13 21:56:26 +0545

    Added search bar

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -19,6 +19,26 @@ const App = () => {
 
   return (
     <>
+
+<div className="container">
+	  <div className="row justify-content-center">
+                        <div className="col-12 col-md-10 col-lg-8">
+                            <form className="card card-sm">
+                                <div className="card-body row no-gutters align-items-center">
+                                    <div className="col-auto">
+                                        <i className="fas fa-search h4 text-body"></i>
+                                    </div>
+                                    <div className="col">
+                                        <input className="form-control form-control-lg form-control-borderless" type="search" placeholder="Search topics or keywords" />
+                                    </div>
+                                    <div className="col-auto">
+                                        <button className="btn btn-lg btn-dark" type="submit">Search</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+      </div>
+  </div>
       <WeatherCard />
     </>
   )

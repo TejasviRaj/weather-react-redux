3c0889654846e3952c379c543140e390c0ff184e
Tejasvi Raj Pant <tejpant@gmail.com>
2020-06-14 13:56:52 +0545

    Connected location to LocationDateInfo component

--- src/Components/WeatherCard/SummaryCard/LocationDateInfo/LocationDateInfo.tsx
+++ src/Components/WeatherCard/SummaryCard/LocationDateInfo/LocationDateInfo.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../../../../Reducers/rootReducer';
 
 const LocationDateInfo = () => {
+    const location: string | null = useSelector((state: RootState) => state.location.name)
     return (
         <div className="locDate-container">
             <h2 className="weekDay">Friday</h2>
             <span className="full-date">16 Aug 2020</span>
             <i className="location-icon" data-feather="map-pin"></i>
             <span className="location"></span>
-                        Ktm, NP
+                       {location}
         </div>
     );
 }

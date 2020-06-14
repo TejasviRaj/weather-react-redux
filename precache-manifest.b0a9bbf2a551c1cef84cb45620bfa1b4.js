1aa29ce8ea11dcb282c82c52d75bdb553f199d7f
Tejasvi Raj Pant <tejpant@gmail.com>
2020-06-14 23:34:13 +0545

    Updates

---
+++ precache-manifest.b0a9bbf2a551c1cef84cb45620bfa1b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff2b416a094b37c2a841f2f872950c81",
+    "url": "/weather-react-redux/index.html"
+  },
+  {
+    "revision": "a089f3744f512fc98221",
+    "url": "/weather-react-redux/static/css/main.cc3bb25c.chunk.css"
+  },
+  {
+    "revision": "cc4a1a3bb6e6acf5054c",
+    "url": "/weather-react-redux/static/js/2.effe559e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-react-redux/static/js/2.effe559e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a089f3744f512fc98221",
+    "url": "/weather-react-redux/static/js/main.2dca4268.chunk.js"
+  },
+  {
+    "revision": "65dccfbb19371df149e1",
+    "url": "/weather-react-redux/static/js/runtime-main.383daed6.js"
+  }
+]);

d2432b009ace57b184c2cb5bc4d1e79cf6a5f722
Tejasvi Raj Pant <tejpant@gmail.com>
2020-06-14 16:48:18 +0545

    Extracted SearchBar to a seperate componenet

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -7,6 +7,7 @@ import { fetchWeather } from '../../Actions/Weather/actionCreatorsWeather'
 import { RootState } from '../../Reducers/rootReducer';
 import WeatherCard from '../WeatherCard/WeatherCard';
 import { fetchLocation } from '../../Actions/Location/actionCreatorsLocation';
+import SearchBar from '../SearchBar/SearchBar';
 
 const App = () => {
   const state: RootState = useSelector((state: RootState) => state)
@@ -27,26 +28,7 @@ const App = () => {
 
   return (
     <>
-
-<div className="container">
-	  <div className="row justify-content-center">
-                        <div className="col-12 col-md-10 col-lg-8">
-                            <form className="card card-sm">
-                                <div className="card-body row no-gutters align-items-center">
-                                    <div className="col-auto">
-                                        <i className="fas fa-search h4 text-body"></i>
-                                    </div>
-                                    <div className="col">
-                                        <input className="form-control form-control-lg form-control-borderless" type="search" placeholder="Search topics or keywords" />
-                                    </div>
-                                    <div className="col-auto">
-                                        <button className="btn btn-lg btn-dark" type="submit">Search</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-      </div>
-  </div>
+      <SearchBar />
       <WeatherCard />
     </>
   )

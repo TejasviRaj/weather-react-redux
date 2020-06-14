28efcdab35930b860650d7008d4eebd6cc402620
Tejasvi Raj Pant <tejpant@gmail.com>
2020-06-14 16:00:07 +0545

    Created helper function capitalizeFirstLetter and fetched data in weatherinfo from redux store

--- src/Components/WeatherCard/SummaryCard/WeatherInfo/WeatherInfo.tsx
+++ src/Components/WeatherCard/SummaryCard/WeatherInfo/WeatherInfo.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../../Reducers/rootReducer';
+import capitalizeFirstLetter from '../../../../Helpers/capitalizeFirstLetter';
 
 const WeatherInfo = () => {
+    const temp: number | null = useSelector((state: RootState) => state.weather.daily && state.weather.daily[1].temp.day);
+    const weatherDescription: string = useSelector((state: RootState) => state.weather.daily && state.weather.daily[1].weather[0].description) ?? '';
+
     return (
         <div className="weather-container">
         <i className="weather-icon" data-feather="sun"></i>
-        <h1 className="temperature">33°C</h1>
-        <h3 className="description">Thunderstorm</h3>
+    <h1 className="temperature">{temp}°C</h1>
+        <h3 className="description">{capitalizeFirstLetter(weatherDescription)}</h3>
     </div> 
     );
 }
